a6b7b4bd5ea79f7a7707c0724d333ff3ed299161
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-09-30 23:55:12 -0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Lutfen bir sayi giriniz:");
+int number = int.Parse(Console.ReadLine());
+if (number == 10)//Girilen sayinin 10 esit oldugu durum kontrolu
+
+{
+    Console.WriteLine("Girdiginiz sayi 10 esittir.");
+
+
+}
+else if (number < 10) //Girilen sayinin 10 kucuk oldugu durum kontrolu
+{
+    Console.WriteLine("Girdiginiz sayi 10'dan kucuktur");
+
+}
+else//Girilen sayinin 10 buyuk oldugudu durum kontrolu
+{
+    Console.WriteLine("Girdiginiz sayi 10'dan buyuktur");
+
+}
+//Girilen sayinin cift mi tek mi oldugunu kontrol eden if/else
+if (number % 2 == 0)
+{
+    Console.WriteLine("Girdiğiniz sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı tektir.");
+}
